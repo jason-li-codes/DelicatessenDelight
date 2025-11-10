991a1b8274de5314586ae07d22a595df68533f76
lijas <lijason991@gmail.com>
2025-11-09 23:02:09 -0500

    starting UserInterface and ReceiptWriter

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import com.pluralsight.utilizedclasses.UserInterface;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+
 }
---
+++ src/main/java/com/pluralsight/utilizedclasses/UserInterface.java
@@ -0,0 +1,27 @@
+package com.pluralsight.utilizedclasses;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private CustomerOrder customerOrder;
+    private Scanner input;
+
+    private void init() {
+        this.customerOrder = new CustomerOrder();
+        input = new Scanner(System.in);
+    }
+
+    public void display() {
+
+
+
+
+
+
+    }
+
+
+
+}
